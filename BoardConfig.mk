6806b7321308557fdce51cabb70b0c04ee29c1a3
RolanDroid <rolandosimoncelli8@gmail.com>
2017-03-05 22:25:08 +0100

    harpia: Use proper toolchains
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_harpia
 
 # Kernel
 TARGET_KERNEL_CONFIG := harpia_defconfig
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-androidkernel-
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216     # 16384 * 1024 mmcblk0p31
