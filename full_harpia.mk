a84043d89f39f2d2c0aea1f98415fd02af46db9e
RolanDroid <rolandosimoncelli8@gmail.com>
2017-03-08 15:30:56 +0100

    harpia: Add screen_size
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- full_harpia.mk
+++ full_harpia.mk
@@ -17,6 +17,10 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := harpia
 PRODUCT_NAME := full_harpia
